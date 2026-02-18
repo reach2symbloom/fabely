240451bca40776e8771166765552d996763237f3
reach2symbloom <reach2symbloom@gmail.com>
2026-02-18 17:03:39 -0600

    setup

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -63,11 +63,7 @@ export function Navbar() {
   await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/auth/callback?next=/`,
-      queryParams: {
-        access_type: "offline",
-        prompt: "consent",
-      },
+      redirectTo: `${window.location.origin}/auth/callback`,
     },
   })
 }
